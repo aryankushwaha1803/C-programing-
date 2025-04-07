1acdb3f2ae61bc22f1934bd7837cfb0589fe9f58
aryanmaurya1803 <aryankushwaha3245@gmail.com>
2025-04-07 07:31:33 +0530

    Add files via upload

---
+++ logical operator.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main(){
+    printf("%d \n",5>3 && 7>6 );//and
+    printf("%d \n",4>3 && 4>8);//and
+    printf("%d \n",6>3 || 7<4);//or
+    printf("%d \n",!(5>7));//not
+    printf("%d \n",!(5>3));//not
+    return 0;
+
+}
