7a861b589f71c1069ef7aef7c6d288f445292fee
aryanmaurya1803 <aryankushwaha3245@gmail.com>
2025-04-08 07:17:46 +0530

    Add files via upload

---
+++ assignment operators.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main(){
+    int a=4;
+    int b=5;
+    a*=b;
+    printf("%d \n",a);
+    int c=4;
+    int d=5;
+    c+=d;
+    printf("%d \n",c);
+    int e=4;
+    int f=5;
+    e-=f;
+    printf("%d \n",e);
+    int g=4;
+    int h=5;
+    g/=h;
+    printf("%d \n",g);
+    int i=4;
+    int j=5;
+    i%=j;
+    printf("%d \n",i);
+    return 0;
+}
