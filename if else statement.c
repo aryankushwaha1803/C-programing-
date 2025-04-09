9900fd948be8c5ce75b9a05be3fb712186ed40db
aryanmaurya1803 <aryankushwaha3245@gmail.com>
2025-04-09 07:37:02 +0530

    Add files via upload

---
+++ if else statement.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+int main(){
+    int a;
+    printf("enter your number");
+    scanf("%d ",&a);
+
+    if(a%2==0){
+        printf("even number \n");
+    }
+    else{
+        printf("odd number");
+    }
+    int getch();
+    return 0;
+
+
+
+
+}
