034d6f6145506a856fde3650d440f96c9006e4cb
aryan kushwaha <aryankushwaha3245@gmail.com>
2025-05-01 07:19:30 +0530

    Create even odd number.c

---
+++ even odd number.c
@@ -0,0 +1,13 @@
+\\find even odd number
+# include<stdio.h>
+int main(){
+    int num;
+    printf("enter your number");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("mumber is even number");
+    }
+    else{
+        printf("number is odd number");
+    }return 0;
+}
